8f45dae553cfe048c85f17604193c564ec014b28
Chaisneau <chaisneau@gmail.com>
2025-12-07 14:02:35 +0100

    priority managed

--- srcs/TokenBlock.hpp
+++ srcs/TokenBlock.hpp
@@ -8,13 +8,13 @@ private:
     bool func_xor(bool a, bool b);
     bool func_or(bool a, bool b);
     bool func_and(bool a, bool b);
+    void executeNot();
+    void executeOthers(char op_target, bool (TokenBlock::*func_op)(bool, bool));
     unsigned int priority;
 
 public:
     TokenBlock(unsigned int priority);
     ~TokenBlock();
-    void executeNot();
-    void executeOthers(char op_target, bool (TokenBlock::*func_op)(bool, bool));
     bool execute();
     unsigned int getPriority() const;
     void setPriority(unsigned int p);
