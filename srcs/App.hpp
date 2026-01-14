65b3e84941bfb858ab2240d2e5fe1a62e02fdd12
Adrien Chaisneau <achaisne@f2r3s7.clusters.42paris.fr>
2026-01-14 18:20:54 +0100

    App class fully commented

--- srcs/App.hpp
+++ srcs/App.hpp
@@ -11,12 +11,26 @@ public:
     int run(int argc, char **argv);
 
 private:
-    static bool parseInteractiveFacts(const std::string &line, std::set<char> &facts);
+    /**
+     * Verify the program is well used
+     */
     static bool isUsageCorrect(int argc, char **argv);
+    /**
+     * Parse the option of the programs, verifying their usage is respected
+     */
     bool parseArgs(int argc, char **argv);
+    /**
+     * Permit to set new inital facts using the stdinput
+     */
     int runInteractive(Parser &parser, Resolver &resolver);
-
+    /**
+     * Parse an interactive facts line into a set of symbols. 
+     */ 
+    static bool parseInteractiveFacts(const std::string &line, std::set<char> &facts);
+    // file path to manage (file containing the facts and logics links)
     std::string input_path;
+    // debug mode activation
     bool print_trace = false;
+    // interactive mode activation
     bool interactive_mode = false;
 };
